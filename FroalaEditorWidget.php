a6e060c43c5f74b8ed685c927244f52fdd30271c
Phan Xuan Dung <dungxuanphan@vccorp.vn>
2015-03-14 12:40:05 +0700

    Fix jquery not function

--- FroalaEditorWidget.php
+++ FroalaEditorWidget.php
@@ -43,7 +43,6 @@ class FroalaEditorWidget extends InputWidget
         }
         $this->registerClientScript();
     }
-
     /**
      * register client scripts(css, javascript)
      */
@@ -52,17 +51,32 @@ class FroalaEditorWidget extends InputWidget
         $view = $this->getView();
         /*
          * Language fix
-         * @author <https://github.com/sim2github>
+         * @author <https://github.com/dungphanxuan>
          */
         if (!isset($this->options['lang']) || empty($this->options['lang'])) {
             $this->options['lang'] = strtolower(substr(Yii::$app->language, 0, 2));
         }
         $options = empty($this->options) ? '' : Json::encode($this->options);
         $asset = FroalaEditorAsset::register($view);
+        $id = $this->options['id'];
 
+        $this->updateAsset();
+        $view ->registerJsFile('http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js');
+        $view ->registerCssFile('http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css');
         $js = "
+         $('#".$id ."').editable({inlineMode: false, alwaysBlank: true});
         ";
         $view->registerJs($js);
+    }
+    /*
+     *
+     * */
+    public function updateAsset(){
+//Replace jquery 2
+        Yii::$app->assetManager->bundles['yii\web\JqueryAsset'] = [
+            'sourcePath' => null,
+            'js' => ['jquery.js' => 'http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js'],
+        ];
     }
 
 }
